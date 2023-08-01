a5f1cda4913264fa1eab143f09207f203260e853
2020105031032-1 <2020105031032-1@VHA-LABCAD-27>
2023-08-01 08:58:04 -0400

    Adicionar arquivos de projeto.

---
+++ Orientada a objetos/Orientada a objetos/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orientada_a_objetos
+{
+    public class Conta
+    {
+        //Atributos da classe conta
+        public string nome;
+
+    }
+}
