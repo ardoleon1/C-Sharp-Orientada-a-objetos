6b5925a421217cc0528c65d2ecf2fd77aa102ec5
ardoleon_ <loen.ribas20@gmail.com>
2023-08-31 15:33:28 -0400

    Exercícios completos, feito do 1 até o 4.

---
+++ Lista de Exercícios/Lista de Exercícios/Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Exercícios
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double peso;
+        public double altura;
+
+        public Pessoa (string nome, int idade, double peso, double altura)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public void Envelhecer()
+        {
+            idade++;
+            if (idade <= 20)
+            {
+                altura = altura + 0.5;
+            }
+        }
+        public void Engordar(double valor)
+        {
+            peso = peso + valor;
+        }
+        public void Emagrecer(double valor)
+        {
+            peso = peso - valor;
+        }
+        public void Crescer(double valor)
+        {
+            altura = altura + valor;
+        }
+    }
+}
--- Lista de Exercícios/Lista de Exercícios/Program.cs
+++ Lista de Exercícios/Lista de Exercícios/Program.cs
@@ -24,12 +24,37 @@ namespace Lista_de_Exercícios
             Console.Write("Digite um valor: ");
             double valor = double.Parse(Console.ReadLine());
             quadrado.MudarValorLado(valor);
-            Console.WriteLine("Perímetro: " + quadrado.CalcularPerimetro());
-            Console.WriteLine("Área: " + quadrado.CalcularArea());
+            Console.WriteLine("Perímetro: " + quadrado.CalcularPerimetro() + "cm");
+            Console.WriteLine("Área: " + quadrado.CalcularArea() + "cm²");
 
             Console.WriteLine("\n---Retangulo---");
             Retangulo retangulo = new Retangulo(0.0, 0.0);
-            Console.WriteLine("");
+            Console.Write("Informe o valor do comprimento em cm: ");
+            double lado1 = double.Parse(Console.ReadLine());
+            Console.Write("Informe o valor do largura em cm: ");
+            double lado2 = double.Parse(Console.ReadLine());
+            retangulo.MudarLadoA(lado1);
+            retangulo.MudarLadoB(lado2);
+            double totalArea = retangulo.CalcularArea();
+            Console.Write("Digite o tamanho do porcelanato em cm²: ");
+            double porcelanato = double.Parse(Console.ReadLine());
+            double quantidadePor = totalArea / porcelanato;
+            Console.Write("Digite o comprimento do rodapé: ");
+            double rodape = double.Parse(Console.ReadLine());
+            double totalPerimetro = retangulo.CalcularPerimetro();
+            double quantidadeRod = totalPerimetro / rodape;
+            Console.WriteLine("\nA área total é " + totalArea + " cm²." + "É necessário " + quantidadePor + " itens de porcelanatos e " + quantidadeRod + " itens de rodapés.");
+
+            Console.WriteLine("\n---Pessoa---");
+            Pessoa pessoa1 = new Pessoa("Gabriel", 16, 55.0, 170.0);
+            pessoa1.Envelhecer();
+            pessoa1.Engordar(5.0);
+            pessoa1.Emagrecer(4.0);
+            pessoa1.Crescer(5.0);
+            Console.WriteLine("Nome: " + pessoa1.nome);
+            Console.WriteLine("Idade: " + pessoa1.idade);
+            Console.WriteLine("Peso: " + pessoa1.peso + "kg");
+            Console.WriteLine("Altura: " + pessoa1.altura + "cm");
 
             Console.ReadKey();
         }
