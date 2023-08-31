08bf997458163bba6390a8601caeca5dca76a009
ardoleon_ <loen.ribas20@gmail.com>
2023-08-31 16:11:54 -0400

    Requisitos atendidos porém está incompleto

---
+++ Bomba de Combustível/Bomba de Combustível/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomba_de_Combustível
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 5---");
+            Console.WriteLine("Seja bem-vindo(a) ao Posto Douglas Legramante!");
+            int cont = 0;
+            {
+                Console.WriteLine("O que você deseja? Escolhe uma das alternativas:");
+                Console.WriteLine("[1] Abastecer por valor\n[2] Abastecer por Litro\n[3] Alterar Valor\n[4] Alterar Tipo de Combustível\n[5] Alterar Quantidade de Combustível\n[6] Sair");
+                cont = int.Parse(Console.ReadLine());
+                switch (cont)
+                {
+
+                }
+
+            } while (cont != 6);
+            Console.WriteLine("Tenha um bom dia!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bomba de Combustível/Bomba de Combustível/bombaCombustível.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomba_de_Combustível
+{
+    public class bombaCombustível
+    {
+        public string tipoCombustivel;
+        public double valorLitro;
+        public double quantidadeCombustivel;
+
+        public void abastecerPorValor(double valor)
+        {
+            double total = valor / valorLitro;
+            Console.WriteLine("Quantidade de litros: " + total);
+            quantidadeCombustivel = quantidadeCombustivel - total;
+        }
+        public void abastecerPorLitro(double valor)
+        {
+            double total = valor * valorLitro;
+            Console.WriteLine("Valor total: " + total);
+            quantidadeCombustivel = quantidadeCombustivel - total;
+        }
+        public void alterarValor(double valor)
+        {
+            valorLitro = valor;
+        }
+        public void alterarCombustivel(string nome)
+        {
+            tipoCombustivel = nome;
+        }
+        public void alterarQuantidadeCombustivel(double valor)
+        {
+            quantidadeCombustivel = valor;
+        }
+    }
+}
