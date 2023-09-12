c323325cc71ca24f29f7888eecb701d014bf90cd
ardoleon_ <loen.ribas20@gmail.com>
2023-09-12 09:15:10 -0400

    Exercício sobre áreas incompleto

---
+++ CalcularArea/CalcularArea/Retangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularArea
+{
+    public class Retangulo
+    {
+        private double x;
+        private double y;
+        private double area;
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Area { get; set; }
+
+    }
+}
