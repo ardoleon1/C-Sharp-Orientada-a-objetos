dc30357346b07c2cd08b0071949e6c54ab096b2c
ardoleon_ <loen.ribas20@gmail.com>
2023-09-26 08:39:23 -0400

    Resolução da prova prática

---
+++ Prova Prática/Prova Prática/BombaDeCombustivel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_Prática
+{
+    public class BombaDeCombustivel
+    {
+        private string tipoCombustivel;
+        private double valorLitro;
+        private double quantidadeCombustivel;
+
+        public string TipoCombustivel { get; set; }
+        public double ValorLitro { get; set; }
+        public double QuantidadeCombustivel 
+        { 
+            get
+            {
+                return quantidadeCombustivel;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    quantidadeCombustivel = value;
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido...");
+                }
+            }
+        }
+
+        public BombaDeCombustivel(string tipoCombustivel, double valorLitro, double quantidadeCombustivel)
+        {
+            TipoCombustivel = tipoCombustivel;
+            ValorLitro = valorLitro;
+            QuantidadeCombustivel = quantidadeCombustivel;
+        }
+        public void AbastecerPorValor(double valor)
+        {
+            double total = valor / ValorLitro;
+            Console.WriteLine("Operação realizada! Foi retirado " + total + " litros!");
+            QuantidadeCombustivel = QuantidadeCombustivel - total;
+            Console.WriteLine("Quantidade de Combustível disponível: " + QuantidadeCombustivel);
+        }
+        public void AbastecerPorLitro(double valor)
+        {
+            double total = valor * ValorLitro;
+            Console.WriteLine("Operação realizada! Valor total é " + total.ToString("c2"));
+            QuantidadeCombustivel = QuantidadeCombustivel - total;
+            Console.WriteLine("Quantidade de Combustível disponível: " + QuantidadeCombustivel);
+        }
+        public void alterarValor(double valor)
+        {
+            ValorLitro = valor;
+        }
+        public void alterarCombustivel(string nome)
+        {
+            TipoCombustivel = nome;
+            Console.WriteLine("Agora o tipo de Combustível é " + TipoCombustivel);
+        }
+        public void alterarQuantidadeCombustivel(double valor)
+        {
+            QuantidadeCombustivel = valor;
+            Console.WriteLine("Agora a quantidade de Combustível é " + QuantidadeCombustivel);
+        }
+    }
+}
---
+++ Prova Prática/Prova Prática/Program.cs
@@ -0,0 +1,111 @@
+﻿//Nomes: Leonardo Pereira Mendes Ribas | Patricia de O. Silva 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_Prática
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Prova Prática---");
+            Console.WriteLine("Seja bem-vindo(a) ao Posto Douglas Legramante!\nOs frentistas Leonardo e Patrícia irão te ajudar!");
+            BombaDeCombustivel bomba = new BombaDeCombustivel("Gasolina", 5, 1000);
+            double valor;
+            string tipo;
+            int cont = 7;
+            do
+            {
+                Console.WriteLine("\nO que você deseja? Escolhe uma das alternativas:");
+                Console.WriteLine("[1] Abastecer por Valor\n[2] Abastecer por Litro\n[3] Alterar Valor\n[4] Alterar Tipo de Combustível\n[5] Alterar Quantidade de Combustível\n" +
+                    "[6] Sair\n");
+                cont = int.Parse(Console.ReadLine());
+                switch (cont)
+                {
+                    case 1:
+                        do
+                        {
+                            Console.Write("\nDigite o valor: ");
+                            valor = double.Parse(Console.ReadLine());
+                            if (valor <= 0)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Valor inválido!");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                bomba.AbastecerPorValor(valor);
+                            }
+                        } while (valor <= 0);
+                        break;
+                    case 2:
+                        do
+                        {
+                            Console.Write("\nDigite a quantidade de litro(s): ");
+                            valor = double.Parse(Console.ReadLine());
+                            if (valor <= 0)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Valor inválido!");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                bomba.AbastecerPorLitro(valor);
+                            }
+                        } while (valor <= 0);
+                        break;
+                    case 3:
+                        do
+                        {
+                            Console.Write("\nDigite o valor: ");
+                            valor = double.Parse(Console.ReadLine());
+                            if (valor <= 0)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Valor inválido!");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                bomba.alterarValor(valor);
+                            }
+                        } while (valor <= 0);
+                        break;
+                    case 4:
+                        Console.Write("\nDigite o Tipo de Combustível: ");
+                        tipo = Console.ReadLine();
+                        Console.Clear();
+                        bomba.alterarCombustivel(tipo);
+                        break;
+                    case 5:
+                        do
+                        {
+                            Console.Write("\nDigite a quantidade: ");
+                            valor = double.Parse(Console.ReadLine());
+                            if (valor <= 0)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Valor inválido!");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                bomba.alterarQuantidadeCombustivel(valor);
+                            }
+                        } while (valor <= 0);
+                        break;
+                }
+
+            } while (cont != 6);
+            Console.Clear();
+            Console.WriteLine("Tenha um bom dia!");
+
+            Console.ReadKey();
+        }
+    }
+}
