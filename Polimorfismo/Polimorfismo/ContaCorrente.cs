54cb1e958e4cf80f24a224935ffb9205b35ea80b
ardoleon_ <loen.ribas20@gmail.com>
2023-10-05 16:15:02 -0400

    Exercício de polimorfismo parcialmente concluída

---
+++ Polimorfismo/Polimorfismo/ContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class ContaCorrente : Conta
+    {
+        private double limite;
+
+        public double Limte { get { return limite; } set { limite = value; } }
+    }
+}
---
+++ Polimorfismo/Polimorfismo/GeradorDeExtrato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class GeradorDeExtrato
+    {
+        public void ImprimeExtratoBasico(Conta c)
+        {
+            DateTime agora = DateTime.Now;
+            string horario = String.Format("{0:d/M/yyyy HH:mm:ss}", agora);
+
+            System.Console.WriteLine("DATA: " + horario);
+            System.Console.WriteLine("SALDO: " + c.Saldo);
+        }
+    }
+}
